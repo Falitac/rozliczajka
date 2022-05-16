1c47a91aca1a8c1d3656d1cf88c962fdaae8c296
Falitac <fastkonrad@gmail.com>
2022-05-16 10:07:17 +0200

    hmm

--- database.php
+++ database.php
@@ -8,6 +8,7 @@
   try {
     $pdo = new PDO("mysql:host=$db_server_name;dbname=$db_name", $db_username, $db_password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    unset($db_server_name, $db_username, $db_password);
   } catch(PDOException $exception) {
     echo "<p>Connection failed</p>";
   }
