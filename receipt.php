b96da1988c252fdd8a04326efdc25719dc14dda1
Falitac <fastkonrad@gmail.com>
2022-10-12 22:23:57 +0200

    divided item to other file, added sum up payment alignment, image sending new logo

--- receipt.php
+++ receipt.php
@@ -4,6 +4,7 @@ use function PHPSTORM_META\type;
 
 require_once('account.php');
 require_once('database.php');
+require_once('item.php');
 
 class Receipt {
   public $payerID;
@@ -361,70 +362,6 @@ class Receipt {
       }
     }
     return FALSE;
-  }
-}
-
-class Item {
-  public $name;
-  public $price;
-  public $payers;
-
-  public function __construct($name = "Default", $price = 0) {
-    $this->name = $name;
-    $this->setPrice($price);
-    $this->payers = array();
-  }
-
-  public function addEveryoneFromReceipt($receipt) {
-    foreach($receipt->personList as $person) {
-      $this->addParticipant($person);
-    }
-  }
-
-  public function addParticipant($id) {
-    $result = array_search($id, $this->payers);
-    if($result === false) {
-      $this->payers[] = intval($id);
-    }
-  }
-
-  public function removeParticipant($id) {
-    $result = array_search($id, $this->payers);
-    if($result !== false) {
-      array_splice($this->payers, $result, 1);
-    }
-  }
-
-  public function setPrice($price) {
-    $this->price = max(0, intval($price));
-  }
-
-  public function setName($name) {
-    if($name === '') {
-      $this->name = 'default_name';
-      return;
-    }
-    $this->name = $name;
-  }
-
-  public function getName() {
-    return $this->name;
-  }
-
-  public function getPrice() {
-    return $this->price;
-  }
-
-  public function getPayers() {
-    return $this->payers;
-  }
-
-  public function getSharedPrice() {
-    $personCount = count($this->payers);
-    if($personCount === 0) {
-      return -1;
-    }
-    return intdiv($this->price , $personCount);
   }
 }
 
